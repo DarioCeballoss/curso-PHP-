95e30be68ef9b7554a95954dacbfa1cf100b6082
Dario Felipe Ceballos Flores <40604215+DarioCeballoss@users.noreply.github.com>
2020-08-02 21:57:47 -0300

    Día 2
    Funciones en otra hoja php

--- index.php
+++ index.php
@@ -8,15 +8,23 @@
 <body>
 
     <?php
-        echo "Este es el primer mensaje <br>" ;
-function dameDatos(){
-    echo "Este es el mensaje del interior de la funcion <br>" ;
 
-}
-dameDatos();
-dameDatos();
+        //datosDeOtraHoja(); da error, ya que la invoca la hoja a partir de la linea 22
+        echo "Este es el primer mensaje <br>" ;
+        function dameDatos(){
+            echo "Este es el mensaje del interior de la funcion <br>" ;
+
+        }
+
+        dameDatos();
+        dameDatos();
+
+        include ("proporciona_datos.php");
+        datosDeOtraHoja();
+
 
         echo "Este es el primer mensaje <br>" ; 
+    
     ?>
 
 
---
+++ proporciona_datos.php
@@ -0,0 +1,8 @@
+<?php
+
+function datosDeOtraHoja(){
+    echo "Este mensaje de encuentra en una funfion en una pagina externa <br>" ;
+
+}
+
+?>
