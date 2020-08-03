f5972bfba147748e340b98409a9c127301fc31e0
Dario Felipe Ceballos Flores <40604215+DarioCeballoss@users.noreply.github.com>
2020-08-02 22:09:40 -0300

    Dia 2
    FUNCIONES 
    TRAER HOJAS PHP ( include y require

--- index.php
+++ index.php
@@ -8,15 +8,25 @@
 <body>
 
     <?php
-        echo "Este es el primer mensaje <br>" ;
-function dameDatos(){
-    echo "Este es el mensaje del interior de la funcion <br>" ;
 
-}
-dameDatos();
-dameDatos();
+        //datosDeOtraHoja(); da error, ya que la invoca la hoja a partir de la linea 22
+        echo "Este es el primer mensaje <br>" ;
+        function dameDatos(){
+            echo "Este es el mensaje del interior de la funcion <br>" ;
+
+        }
+
+        dameDatos();
+        dameDatos();
+
+       // include ("proporciona_datos.php");   // incluye el archivo, si no esta no hay problema
+        require ("proporciona_datos.php");   // con require el archivo si o si tene que estar para continuar ejecutando 
+
+        datosDeOtraHoja();
+
 
         echo "Este es el primer mensaje <br>" ; 
+    
     ?>
 
 
