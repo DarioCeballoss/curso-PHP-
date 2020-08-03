93fcdb718f7024e659fab50e731730b1f2a75b83
Dario Felipe Ceballos Flores <40604215+DarioCeballoss@users.noreply.github.com>
2020-08-02 21:17:56 -0300

    Día 2
    comentarios, variables , print y echo

--- index.php
+++ index.php
@@ -7,10 +7,17 @@
 </head>
 <body>
     <?php
-        print "Hola mundo <br>";
-        print " :) ";
+        $nombre = "Dario" ; // crea la variable nombre con valor dario
+        $edad = 30 ; 
+        print "El nombre del usiuario es: $nombre <br>";
+        print "El nombre del usiuario es: " . $nombre;
+        print ' <br> El nombre del usiuario es: $nombre <br><br>' ;
 
+        print "El nombre del usiuario es: $nombre  $edad <br>";
+        print "El nombre del usiuario es: " . $nombre . $edad . " <br>";
+        print ' El nombre del usiuario es: $nombre $edad <br>';
 
+        echo $nombre, $edad ; 
     ?>
 
 
